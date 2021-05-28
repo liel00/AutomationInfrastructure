ca9c14e5963355300c037a4c29b78478c2b8ee36
liel00 <62212244+liel00@users.noreply.github.com>
2021-05-28 16:07:33 +0300

    Update test_one.py

--- test/test_one.py
+++ test/test_one.py
@@ -2,7 +2,7 @@ from page_objects.one_page import OneTest
 from utilities import Base
 
 
-class TestOne(Base):
+class TestOne(Base.Base):
     def test_one(self):
         one = OneTest(self.driver)
         one.click_search()
